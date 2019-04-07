d8bf80ce7426eee9609e8003931588864ff35b0f
lessless <yevhenii.kurtov@gmail.com>
2019-04-08 00:32:31 +0200

    Format and include cowboy 1

--- mix.exs
+++ mix.exs
@@ -1,6 +1,18 @@
 defmodule Sample.Mixfile do
   use Mix.Project
-  def project do [ app: :n2o_elixir, version: "0.0.1", deps: deps ] end
-  def application do [ mod: { Sample, [] }, applications: [:cowboy, :n2o] ] end
-  defp deps do [ {:n2o, github: "5HT/n2o"} ] end
+
+  def project() do
+    [app: :n2o_elixir, version: "0.0.1", deps: deps()]
+  end
+
+  def application() do
+    [mod: {Sample, []}, applications: [:cowboy, :n2o]]
+  end
+
+  defp deps() do
+    [
+      {:n2o, github: "5HT/n2o", manager: :rebar}, 
+      {:cowboy, "~> 1.0"},
+    ]
+  end
 end

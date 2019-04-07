781f56c5e2274e026e3c16cfd59cc6f2a9dbbfb9
lessless <yevhenii.kurtov@gmail.com>
2019-04-08 00:33:01 +0200

    Format and update behaviour

--- lib/sample.ex
+++ lib/sample.ex
@@ -1,16 +1,23 @@
 defmodule Sample do
+  use Application
 
-    use Application.Behaviour
-    def start(_type, _args) do
-        :application.set_env(:n2o, :route, Sample.Routes)
-        dispatch = :cowboy_router.compile([{:_,
-            [{"/static/[...]", :cowboy_static, [{:directory, :wf.to_list(__DIR__) ++ "/priv/static"},
-                                               {:mimetypes, {&:mimetypes.path_to_mimes/2, :default}}]},
-            {"/rest/:resource",     :n2o_cowboy_rest, []},
-            {"/rest/:resource/:id", :n2o_cowboy_rest, []},
-            {"/ws/[...]",           :bullet_handler, [{:handler, :n2o_bullet}]},
-            {:_, :n2o_cowboy, []}] }])
-        {:ok, _} = :cowboy.start_http(:http, 100, [port: 8080], [env: [dispatch: dispatch]])
-        Sample.Sup.start_link end
+  def start(_type, _args) do
+    :application.set_env(:n2o, :route, Sample.Routes)
 
+    dispatch =
+      :cowboy_router.compile([
+        {:_,
+         [
+           {"/static/[...]", :cowboy_static,
+            [{:directory, :wf.to_list(__DIR__) ++ "/priv/static"}, {:mimetypes, {&:mimetypes.path_to_mimes/2, :default}}]},
+           {"/rest/:resource", :n2o_cowboy_rest, []},
+           {"/rest/:resource/:id", :n2o_cowboy_rest, []},
+           {"/ws/[...]", :bullet_handler, [{:handler, :n2o_bullet}]},
+           {:_, :n2o_cowboy, []}
+         ]}
+      ])
+
+    {:ok, _} = :cowboy.start_http(:http, 100, [port: 8080], env: [dispatch: dispatch])
+    Sample.Sup.start_link()
+  end
 end
